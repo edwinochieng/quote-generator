838d72654b1abf2b7f7b226e1af61b90b5b2039f
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-17 02:35:03 +0300

    add new custom width and custom font class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '128': '36rem',
+      },
+      fontFamily :{
+        'quicksand': 'Quicksand',
+        'tangerine': 'Tangerine',
+
+      }
+    },
   },
   plugins: [],
 }
