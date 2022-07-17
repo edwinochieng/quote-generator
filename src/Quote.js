77b158905fbbeabf8be641f4f181ff9d795cea4f
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-17 05:56:12 +0300

    link new quote button to api call function

--- src/Quote.js
+++ src/Quote.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-function Quote() {
+function Quote(props) {
   return (
-    <div className="h-72 w-128 bg-yellow-200  border border-solid border-2 border-black rounded-2xl">
-      <div className="py-16 px-20">
+    <div className="relative h-80 w-128 bg-yellow-200  border border-solid border-2 border-black rounded-2xl flex flex-col items-center">
+      <div className="absolute pt-10 px-4">
         <p className="font-tangerine font-semibold text-3xl ">
-          Hello my name is Edwin Ochieng I go to JKUAT
+         " {props.quote} "
         </p>
       </div>
+      <div className="absolute bottom-20">
+      <span className="font-quicksand text-lg ">{props.author}</span>
+      </div>
 
-      <div className="pt-8 px-60">
-        <button className="border border-solid border-2 border-black rounded-lg h-12 w-36 font-semibold font-quicksand text-black text-lg hover:bg-black hover:text-yellow-200">
+      <div className="absolute bottom-2">
+        <button className="border border-solid border-2 border-black rounded-lg h-12 w-36 font-semibold font-quicksand text-black text-lg hover:bg-black hover:text-yellow-200"
+         onClick={props.onAdd}   
+        >
           New Quote
         </button>
       </div>
