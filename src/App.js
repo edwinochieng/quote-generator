df0b3472ec9188edcb2f3da2ce47bf49d95a9a88
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-17 04:03:18 +0300

    add api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Quote from "./Quote";
 function App() {
+
+  useEffect(()=>{
+    getQuote();
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fda4f5d54dmsh202f703a9d1e7b2p10f3a0jsn191b31350837',
+      'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+    }
+  };
+  
+  const getQuote = async () =>{
+
+      const api = await fetch('https://famous-quotes4.p.rapidapi.com/random?category=all&count=2', options);
+      const data = await api.json();
+
+      console.log(data);
+  }
   return (
-    <div >
-      Hello
+    <div className="flex flex-col items-center pt-36 bg-yellow-100 h-screen">
+      <Quote/>
     </div>
   );
 }
